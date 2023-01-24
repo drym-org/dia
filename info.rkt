278207de292eddbdc8bf9b3b5ed2eca63c8265c9
Siddhartha <sid@countvajhula.com>
2023-01-24 00:35:24 -0800

    add info.rkt to make the code installable

---
+++ info.rkt
@@ -0,0 +1,8 @@
+#lang info
+
+(define version "0.0")
+(define collection "abe")
+(define deps '("base"))
+(define build-deps '())
+(define clean '("compiled" "private/compiled"))
+(define pkg-authors '(countvajhula))
