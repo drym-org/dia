02c17742c6a880bddb67bf26f21ba302deb720ae
Siddhartha <sid@countvajhula.com>
2023-01-28 20:07:05 -0800

    declare missing package dependencies

--- info.rkt
+++ info.rkt
@@ -2,7 +2,9 @@
 
 (define version "0.0")
 (define collection "abe")
-(define deps '("base"))
+(define deps '("base"
+               "qi-lib"
+               "relation-lib"))
 (define build-deps '())
 (define clean '("compiled" "private/compiled"))
 (define pkg-authors '(countvajhula))
