68b2b2d884b0f5525307bd915e426f738c925119
Siddhartha <sid@countvajhula.com>
2023-04-19 17:12:33 -0700

    A CLI script to convert an RKTD file to CSV

---
+++ tools/rktd2csv.rkt
@@ -0,0 +1,27 @@
+#!/usr/bin/env racket
+#lang cli
+
+(require csv-writing
+         qi
+         relation)
+
+(define (read-rktd filename)
+  (call-with-input-file filename
+    (λ (port)
+      ;; the input is a list of conses
+      ;; but we need a "table" which is
+      ;; a list of lists
+      (map (☯ (~> (-< (~> car ->string) cdr)
+                  list))
+           (eval (read port) (make-base-namespace))))))
+
+(define (write-csv data [headers #f])
+  (let ([data (if headers
+                  (cons headers data)
+                  data)])
+    (display-table data)))
+
+(program (main [rktd-filename "The RKTD file (e.g. an attributions file that is the output of DIA)."])
+  (write-csv (read-rktd rktd-filename)))
+
+(run main)
